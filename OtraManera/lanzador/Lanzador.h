c6a708bf38ed771efa6cce250b2ae3b4a097c142
junx21 <jxuche@myuax.com>
2024-11-30 21:58:59 +0100

    lanzador

---
+++ OtraManera/lanzador/Lanzador.h
@@ -0,0 +1,14 @@
+#ifndef LANZADOR_H
+#define LANZADOR_H
+
+
+void agregarTareas_Actor();
+
+void listaTareas();
+
+void agregarActores();
+void modificarActor();
+void eliminarActor();
+
+
+#endif //LANZADOR_H
